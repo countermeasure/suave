2a31ab751218b6b1019f36943909bca262d5f9f1
Sky <sky@skychristensen.com>
2015-10-31 16:39:47 +1100

    Grid arithmetic is clearer

--- grid.py
+++ grid.py
@@ -13,45 +13,39 @@ class Grid(object):
     def column_width(self):
         """Get the column width."""
         screen_width = self.screen.getmaxyx()[1]
-        return (
-            (screen_width - (self.gutter_width * (self.no_columns + 1))) / 12
-        )
+        width_of_gutters = self.gutter_width * (self.no_columns + 1)
+        return ((screen_width - width_of_gutters) / self.no_columns)
 
     @property
     def row_height(self):
         """Get the row height."""
         screen_height = self.screen.getmaxyx()[0]
-        return ((screen_height - (self.gutter_width * (self.no_rows + 1))) / 6)
+        height_of_gutters = self.gutter_width * (self.no_rows + 1)
+        return ((screen_height - height_of_gutters) / self.no_rows)
 
     def y_offset(self, rows_offset):
         """
         Returns the y-origin point measured in lines.
         """
-        return (
-            self.gutter_width + (
-                (self.row_height + self.gutter_width) * rows_offset
-            )
-        )
+        row_height_with_gutter = self.row_height + self.gutter_width
+        return (self.gutter_width + (row_height_with_gutter * rows_offset))
 
     def x_offset(self, columns_offset):
         """
         Returns the x-origin point measured in characters.
         """
-        return (
-            self.gutter_width + (
-                (self.column_width + self.gutter_width) * columns_offset
-            )
-        )
+        col_width_with_gutter = self.column_width + self.gutter_width
+        return (self.gutter_width + (col_width_with_gutter * columns_offset))
 
     def height(self, rows):
         """
-        Returns the height point measured in lines.
+        Returns the window height measured in lines.
         """
         return ((self.row_height * rows) + (self.gutter_width * (rows - 1)))
 
     def width(self, columns):
         """
-        Returns the width point measured in characters.
+        Returns the window width measured in characters.
         """
         return (
             (self.column_width * columns) + (self.gutter_width * (columns - 1))
