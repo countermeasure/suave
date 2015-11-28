96af36b1e9ba55f866d51eccdd3cc896f0e40ba5
Sky <sky@skychristensen.com>
2015-11-29 00:37:50 +1100

    Added random line builder

---
+++ suave/builders/random_line.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import argparse
+import random
+import yaml
+
+
+def random_line(file_path):
+    """
+    Returns a randomly-chosen list item from the YAML file at `file_path`.
+    """
+    with open(file_path, 'r') as f:
+        contents = yaml.load(f.read())
+
+    item_no = random.randint(0, (len(contents) - 1))
+
+    return contents[item_no]
+
+
+if __name__ == "__main__":
+    # Accept the file name as the first positional argument.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    args = parser.parse_args()
+
+    # Print a random line from the file.
+    print '\n  %s' % random_line(args.file)
